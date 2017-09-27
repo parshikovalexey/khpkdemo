a9f0937c1889f127363b4b6ec846ebdc5eb9a75a
parshikovalexey <parshikovalexey@gmail.com>
2017-09-27 16:03:25 +0700

    ДЕмонстрационная версия чат-бота для ХПК

---
+++ demo2/Bot Application1/Bot Application1/Dialogs/RootDialog.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Net;
+using System.IO;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace Bot_Application1.Dialogs {
+    [Serializable]
+    public class RootDialog : IDialog<object> {
+        public Task StartAsync(IDialogContext context) {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result) {
+            var activity = await result as Activity;
+
+            // calculate something for us to return
+            int length = (activity.Text ?? string.Empty).Length;
+
+            // return our reply to the user
+            // await context.PostAsync($"Ты написал '{activity.Text}' длино {length} символа(ов)");
+
+            var wikiText = GetDataFromWiki(activity.Text);
+            //var solution = CalculateExpression(activity.Text);
+
+            await context.PostAsync(wikiText);
+
+            context.Wait(MessageReceivedAsync);
+        }
+
+
+        private string CalculateExpression(string expression) {
+            var uriString = $"http://api.mathjs.org/v1/?expr={HttpUtility.UrlEncode(expression)}";
+            return getDataToUrl(uriString);
+        }
+
+
+        private string getDataToUrl(string url) {
+            try {
+                // Create a new WebClient instance.
+                WebClient myWebClient = new WebClient();
+                // Download home page data. 
+                // Open a stream to point to the data stream coming from the Web resource.
+                Stream myStream = myWebClient.OpenRead(url);
+
+                StreamReader sr = new StreamReader(myStream);
+                var result = sr.ReadToEnd();
+                // Close the stream. 
+                myStream.Close();
+                return result;
+            }catch (Exception e) {
+                return string.Empty;
+            }
+        }
+
+        private string PArseWikiResponse(string wikiResponse) {
+            var result = "";
+            var deser = new JavaScriptSerializer().Deserialize<WikiSearchResponse>(wikiResponse);
+            foreach (var item in deser.query.search) {
+                result += "\r\n" + item.snippet;
+            }
+            return result;
+        }
+
+
+        private string GetDataFromWiki(string action) {
+            var uriString = $"https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={action}&utf8=&format=json";
+            var response = getDataToUrl(uriString);
+            return PArseWikiResponse(response);
+        }
+
+    }
+}
---
+++ demo2/Bot Application1/Bot Application1/Models.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_Application1 {
+
+    public class WikiSearchResponse {
+        public string batchcomplete { get; set; }
+        public ContinueObject Continue { get; set; }
+        public QueryObject query { get; set; }
+
+    }
+
+    public class QueryObject {
+        public SearchInfoObject searchinfo { get; set; }
+        public ICollection<SearchObject> search { get; set; }
+    }
+
+    public class SearchObject {
+        public int ns { get; set; }
+        public string title { get; set; }
+
+        public int pageid { get; set; }
+        public int size { get; set; }
+        public int wordcount { get; set; }
+        public string snippet { get; set; }
+        public DateTime timestamp { get; set; }
+    }
+
+    public class SearchInfoObject {
+        public int totalhits { get; set; }
+    }
+
+    public class ContinueObject {
+        public int sroffset { get; set; }
+        public string Continue { get; set; }
+    }
+
+    public class Models {
+    }
+}
